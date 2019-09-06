ae34b8e7bacea56ee6b19403ac3b841becf5a4ba
Youness <youness.acdc@gmail.com>
2019-09-06 20:38:42 +0100

    completed build-task

---
+++ gulp/tasks/build.js
@@ -0,0 +1,64 @@
+var gulp = require('gulp'),
+imagemin = require('gulp-imagemin'),
+del = require('del'),
+usemin = require('gulp-usemin'),
+rev = require('gulp-rev'),
+cssnano = require('gulp-cssnano'),
+uglify = require('gulp-uglify'),
+browserSync = require('browser-sync').create();
+
+
+gulp.task('previewDist', function(){
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "docs"
+    }
+  });
+});
+
+gulp.task('deletDist', ['icons'], function(){
+    return del("./docs");
+});
+
+gulp.task('CopyGeneralFiles', ['deletDist'], function(){
+  var pathstoCopy = [
+    './app/**/*',
+    '!./app/index.html',
+    '!./app/assets/images/**',
+    '!./app/assets/styles/**',
+    '!./app/assets/scripts/**',
+    '!./app/temp',
+    '!./app/temp/**'
+  ]
+
+  return gulp.src(pathstoCopy)
+  .pipe(gulp.dest("./docs"));
+});
+
+
+gulp.task('optimizeImages',['deletDist'], function(){
+  return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
+    .pipe(imagemin({
+      progressive: true,
+      interlaced: true,
+      multipass: true
+    }))
+  .pipe(gulp.dest("./docs/assets/images/"))
+});
+
+gulp.task('useminTrigger', function(){
+  gulp.start("usermin")
+});
+
+gulp.task('usermin', ['scripts', 'styles'], function(){
+  return gulp.src("./app/index.html")
+  .pipe(usemin({
+    css: [function(){return rev()},function(){return cssnano()}],
+    js: [function(){return rev()}, function() {return uglify()}]
+  }))
+  .pipe(gulp.dest("./docs"));
+});
+
+
+gulp.task('build', ['deletDist', 'CopyGeneralFiles', 'optimizeImages', 'useminTrigger']);
